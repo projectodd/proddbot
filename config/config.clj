c2afac62e29bc36677aceff9c68e32271608a5d5
Tobias Crawley <tcrawley@gmail.com>
2012-04-30 21:17:42 -0400

    No need for all of those clojure prefixes.

--- config/config.clj
+++ config/config.clj
@@ -3,9 +3,9 @@
  :max-operations 3 ; The maximum number of operations that can be running at any given time.
  :pending-ops 0    ; The number of operations running right now
  :prefix-arrow "\u21D2 "
- :clojure {:eval-prefixes {:defaults ["->" "." "," ; prefixes in any channel
+ :clojure {:eval-prefixes {:defaults ["->" ; prefixes in any channel
                                       #"&\|(.*?)(?=\|&|\|&|$)" ; stuff like &|this|&
-                                      #"##(([^#]|#(?!#))+)\s*((##)?(?=.*##)|$)"]}}
+                                      ]}}
  "irc.freenode.net" {:channels ["##tcrawley" "#immutant" "#torquebox"]
                      :bot-name "proddbot"
                      :plugins #{"clojure" "javadoc" "jruby" "jira" ;"annoy-jim" "greeting"
