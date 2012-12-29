f9662e10670635cf58f108032c08f32a83efce6c
Toby Crawley <toby@tcrawley.org>
2012-12-29 09:27:39 -0500

    Use my lazybot.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject proddbot-immutant "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lazybot "0.7.0-alpha1"]])
+                 [org.clojars.tcrawley/lazybot "0.7.0-alpha1"]]
+  :immutant {:nrepl-port 4005})
