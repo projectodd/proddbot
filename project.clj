45609b60c86c8828c942bbe7660a9442feab85e9
Toby Crawley <toby@tcrawley.org>
2016-03-07 09:05:45 -0500

    Fix licensing.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject proddbot "2.0.0-SNAPSHOT"
   :description "Yet another IRC bot"
   :url "https://github.com/tobias/proddbot"
-  :license {:name "Apache Software License - v 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"
-            :distribution :repo}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [irclj "0.5.0-alpha4"]]
   :main proddbot.main
