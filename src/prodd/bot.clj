f7191b37ce560be5e6ee77ec52a5fc9f0f31b17e
Toby Crawley <toby@tcrawley.org>
2017-06-21 08:23:13 -0400

    Allow overriding config via sysprops; pull jenkins token from config

--- src/prodd/bot.clj
+++ src/prodd/bot.clj
@@ -31,7 +31,7 @@
    (println event-type "event received, exiting")
    (System/exit 1)))
 
-(defn start [{:keys [host port web-port web-host nick channels] :as config}]
+(defn start [{:keys [host port web-port web-host nick channels jenkins-token] :as config}]
   (let [irc (irc/connect host port nick
               :callbacks {:raw-log events/stdout-callback
                           :privmsg #(#'msg-handler config %1 %2)
@@ -43,7 +43,7 @@
     (builds/start
       web-port
       web-host
-      (System/getenv "JENKINS_TOKEN")
+      jenkins-token
       (set (keys channels))
       #(irc/message irc %1 %2))
     (releases/start config (fn [chan msg] (irc/message irc chan msg)))
@@ -61,6 +61,7 @@
      :web-port 8080
      :web-host "localhost"
      :nick "proddbot2"
+     :jenkins-token "foo"
      :channels {"##tcrawley" {:issue-url "https://issues.jboss.org/browse/IMMUTANT"}}
      :issue-triggers {:directed [#"(?i)^@(jira|issue) ([^\s]+)"]
                       :global [#"(?i)(fil(e|ing)|create) an* (jira|issue)[?]"
--- src/proddbot/main.clj
+++ src/proddbot/main.clj
@@ -6,8 +6,18 @@
 (defn read-config [path]
   (-> path io/file slurp read-string))
 
+(defn override-config-from-sysprops [cfg]
+  (->> (for [[name value] (System/getProperties)
+             :when (.startsWith name "proddbot.")
+             :let [name (keyword (subs name (count "proddbot.")))]]
+         [name value])
+    (into {})
+    (merge cfg)))
+
 (defn -main [& args]
   (if (not= 1 (count args))
     (println "Path to config file should be the only arg")
-    (bot/start (read-config (first args)))))
+    (bot/start (-> (first args)
+                 read-config
+                 override-config-from-sysprops))))
 
